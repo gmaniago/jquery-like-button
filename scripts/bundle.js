cdc70757b71b536124f234d76816487fd2bd841a
Gladys Maniago <gfmaniago@gmail.com>
2015-09-14 14:10:26 -0500

    normal mode complete

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -3,6 +3,23 @@
 
 $(document).ready(start);
 
+function start() {
+
+	var $likeButton = $('#likebtn');
+	var counts = 0;
+
+	$likeButton.on('click', addClick);
+
+	function addClick() {
+		counts++;
+		if (counts === 1) {
+			$likeButton.html(counts + ' Like');
+		} else {
+			$likeButton.html(counts + ' Likes');
+		}
+	}
+}
+
 },{}]},{},[1])
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,24 @@
 'use strict';
 
-$(document).ready(start);
+ $(document).ready(start);
 
+function start(){
+
+var $likeButton = $('#likebtn');
+var counts = 0;
+
+$likeButton.on('click', addClick);
+
+function addClick(){
+	counts++;
+	if(counts === 1){
+			$likeButton.html(counts + ' Like');
+	}
+	else{
+		$likeButton.html(counts + ' Likes');
+	}
+}
+
+
+
+}
